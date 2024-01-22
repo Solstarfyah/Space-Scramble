70333be97c645f435538f92fde8e3fdefc7b5fbb
Solstarfyah <103515124+Solstarfyah@users.noreply.github.com>
2024-01-22 10:04:51 -0500

    resetup

---
+++ Assets/scripts/Scroller.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroller : MonoBehaviour
+{
+    public float speed;
+
+    [SerializeField]
+    private Renderer bgRenderer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
+    }
+}
