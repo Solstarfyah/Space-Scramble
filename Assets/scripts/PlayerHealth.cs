1158631137dc374d117eed9840d6ed22d3084a4a
Sabrina Dini <sabrina.dini02@gmail.com>
2024-01-29 22:45:00 -0500

    Update 1.2
    (i wanted to sound like a video game dev lol)
    - Put everything we have together so far
    - Added health bar
    - Constricted player movement so they can only jump.

---
+++ Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBarScript healthBar;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void OnCollisionEnter2D(UnityEngine.Collision2D collision)
+    {
+        if (collision.gameObject.tag == "obstacle")
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
