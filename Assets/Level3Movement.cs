a86ef223f2fae2ae3774b3fd501cb64f9aaa2dd5
Sabrina Dini <147353096+s-dini@users.noreply.github.com>
2024-02-01 14:47:58 -0500

    working fuel bars

--- Assets/Level3Movement.cs
+++ Assets/Level3Movement.cs
@@ -5,20 +5,36 @@ using UnityEngine;
 public class Level3Movement : MonoBehaviour
 {
     public float moveSpeed;
+    public HealthBarScript healthBar;
+    public int maxFuel;
+
+    [SerializeField]
+    private float fuelBurnRate;
+
+    
+    private int currentFuel;
+    
+    private bool isFuel;
+    private bool isMoving; // checks if player is moving forward
+    
     
     private bool facingRight = true; // sets default player direction 
     private float XmoveDirection;
     private float YmoveDirection;
     private Rigidbody2D rb;
-    
+
+
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>(); // looks for Rigidbody 2D component 
+        rb = GetComponent<Rigidbody2D>(); // looks for Rigidbody 2D component
+        healthBar.SetMaxHealth(maxFuel);
+        currentFuel = maxFuel;
     }
 
     // Update is called once per frame
     void Update()
     {
+        checkMovement();
         ProcessInputs();
     }
 
@@ -28,11 +44,45 @@ public class Level3Movement : MonoBehaviour
         transform.Rotate(0f, 180f, 0f);
     }
 
-    private void ProcessInputs()
+    public void ProcessInputs()
     { 
         XmoveDirection = Input.GetAxis("Horizontal");
         YmoveDirection = Input.GetAxis("Vertical");
         rb.velocity = new Vector2 (XmoveDirection * moveSpeed, YmoveDirection * moveSpeed);
+
+        if(isMoving && isFuel)
+        {
+            currentFuel -= (int)( fuelBurnRate * Time.deltaTime);
+            Debug.Log(currentFuel);
+            healthBar.SetHealth( currentFuel );
+        }
     }
 
+    void OnCollisionEnter2D( UnityEngine.Collision2D col )
+    {
+        if (col.gameObject.tag == "fuel")
+        {
+            AddFuel(20);
+        }
+    }
+
+    void AddFuel( int fuelPoints ) 
+    {
+        currentFuel += fuelPoints;
+        healthBar.SetHealth(currentFuel);
+    }
+
+    void checkMovement()
+    {
+        isMoving = Input.GetKey(KeyCode.D);
+        
+        if (currentFuel != 0)
+        {
+            isFuel = true;
+        }
+        else 
+        {
+            isFuel = false;
+        }
+    }
 }
