6af5c228fac5cc89ba2d251be0d1238f56cbb07e
Solstarfyah <103515124+Solstarfyah@users.noreply.github.com>
2024-02-06 12:05:27 -0500

    disregard main for now

--- Assets/scripts/BlastScript.cs
+++ Assets/scripts/BlastScript.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class BlastScript : MonoBehaviour
 {
-    private Vector3 mousePos;
-    private Camera mainCam;
+    public combatTesting rotZ;
+    public Transform RotatePoint;
+    public Transform BlastOffset;
     private Rigidbody2D rb;
     public float force;
 
     void Start()
     {
-
-        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         rb = GetComponent<Rigidbody2D>();
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePos - transform.position;
-        Vector3 rotation = transform.position - mousePos;
+        Vector3 rotation = RotatePoint.transform.position - BlastOffset.transform.position;
+        Vector3 direction = BlastOffset.transform.position - RotatePoint.transform.position;
         rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
         float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rot + 90);
--- Assets/scripts/CombatTypeSystem.cs
+++ Assets/scripts/CombatTypeSystem.cs
@@ -1,18 +1,248 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
-public class CombatTypeStystem : MonoBehaviour
+public class CombatTypeSystem : MonoBehaviour
 {
+
+    float pbx;
+    float pby;
+    float nextFire;
+
+    [Header("Both")]
+    public int pHealth = 3;
+    public Rigidbody2D rb;
+    public float pFuelBar = 10;
+    [Space(5)]
+
+    [Header("Blast Settings")]
+
+    public Transform blastOffset;
+    public GameObject blastPrefab;
+    public Transform RotatePoint;
+    public float force;
+    [Space(5)]
+
+    public bool canFire;
+    public float blastCD;
+    public float timeBetweenFiring;
+    public float rightOrUp;
+    public float leftOrDown;
+    public float diagonalFire;
+    [Space(5)]
+
+    [Header("Parry Settings")]
+
+    public bool canParry;
+    public bool parried;
+    public float pForce;
+    public float timeBetweenParrying;
+    public float parryBuffer;
+    public float parryTimer = 0;
+    public float parryCD = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        parried = false;
+        canParry = true;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
         
+
+        RotatePoint.transform.rotation;
+        
+        */
+
+
+        if (!canFire)
+        {
+            blastCD += Time.deltaTime;
+            if (blastCD > timeBetweenFiring)
+            {
+                canFire = true;
+                blastCD = 0;
+            }
+        }
+
+        if (Input.GetKey("e") && canFire)
+        {
+            if (pFuelBar >= 1 && canFire == true)
+            {
+                canFire = false;
+                blastCD = 0;
+                fireUpRight();
+                fireUpLeft();
+                fireDownRight();
+                fireDownLeft();
+                //rb.AddForce(rotation * force, ForceMode2D.Impulse);
+                pFuelBar -= 1;
+            }
+        }
+
+        if (!canParry)
+        {
+            parryCD += Time.deltaTime;
+            if (parryCD >= timeBetweenParrying)
+            {
+                canParry = true;
+                parryCD = 0;
+            }
+        }
+
+        if (Input.GetKey("f") && canParry)
+        {
+            parryTimer += Time.deltaTime;
+            if (parryTimer <= parryBuffer)
+            {
+                parried = true;
+                parryTimer += Time.deltaTime;
+            }
+
+            else if (parryTimer >= parryBuffer || Input.GetButton("f") == false)
+            {
+                parried = false;
+                parryTimer = 0;
+                canParry = false;
+            }
+
+        }
+
+        void CreatePlayerBullet()
+        {
+            Instantiate(blastPrefab, blastOffset.position, Quaternion.identity);
+        }
+
+        void fireDownLeft()
+        {
+            if (Input.GetKey("s") && Time.time > nextFire && Input.GetKey("a"))
+            {
+                nextFire = Time.time + timeBetweenFiring;
+                CreatePlayerBullet();
+                pbx = leftOrDown * diagonalFire;
+                pby = leftOrDown * diagonalFire;
+                BulletMovement.velX = pbx;
+                BulletMovement.velY = pby;
+            }
+            else if (Input.GetKey("s") && Time.time > nextFire)
+            {
+                nextFire = Time.time + timeBetweenFiring;
+                CreatePlayerBullet();
+                pbx = 0;
+                pby = leftOrDown;
+                BulletMovement.velX = pbx;
+                BulletMovement.velY = pby;
+            }
+            else if (Input.GetKey("a") && Time.time > nextFire)
+            {
+                nextFire = Time.time + timeBetweenFiring;
+                CreatePlayerBullet();
+                pbx = leftOrDown;
+                pby = 0;
+                BulletMovement.velX = pbx;
+                BulletMovement.velY = pby;
+            }
+        }
+
+
+
+        void fireUpLeft()
+        {
+            if (Input.GetKey("a") && Time.time > nextFire && Input.GetKey("w"))
+            {
+                nextFire = Time.time + timeBetweenFiring;
+                CreatePlayerBullet();
+                pbx = leftOrDown * diagonalFire;
+                pby = rightOrUp * diagonalFire;
+                BulletMovement.velX = pbx;
+                BulletMovement.velY = pby;
+            }
+
+            else if (Input.GetKey("w") && Time.time > nextFire)
+            {
+                nextFire = Time.time + timeBetweenFiring;
+                CreatePlayerBullet();
+                pbx = 0;
+                pby = rightOrUp;
+                BulletMovement.velX = pbx;
+                BulletMovement.velY = pby;
+            }
+        }
+
+        void fireDownRight()
+        {
+            if (Input.GetKey("d") && Time.time > nextFire && Input.GetKey("s"))
+            {
+                nextFire = Time.time + timeBetweenFiring;
+                CreatePlayerBullet();
+                pbx = rightOrUp * diagonalFire;
+                pby = leftOrDown * diagonalFire;
+                BulletMovement.velX = pbx;
+                BulletMovement.velY = pby;
+            }
+            else if (Input.GetKey("d") && Time.time > nextFire)
+            {
+                nextFire = Time.time + timeBetweenFiring;
+                CreatePlayerBullet();
+                pbx = rightOrUp;
+                pby = 0;
+                BulletMovement.velX = pbx;
+                BulletMovement.velY = pby;
+            }
+
+        }
+
+        void fireUpRight()
+        {
+            if (Input.GetKey("d") && Time.time > nextFire && Input.GetKey("w"))
+            {
+
+                nextFire = Time.time + timeBetweenFiring;
+                CreatePlayerBullet();
+                pbx = rightOrUp * diagonalFire;
+                pby = rightOrUp * diagonalFire;
+                BulletMovement.velX = pbx;
+                BulletMovement.velY = pby;
+            }
+
+        }
+
+        void FixedUpdate()
+        {
+            fireUpRight();
+            fireUpLeft();
+            fireDownRight();
+            fireDownLeft();
+        }
+
+        void OnCollisionEnter2D(Collision2D collisionInfo)
+        {
+            if (parried != true)
+            {
+                pHealth -= 1;
+            }
+
+            else if (parried == true)
+            {
+                if (collisionInfo.collider.name == "ObstacleBase(Clone)")
+                {
+                    rb.AddForce(Vector2.up * pForce, ForceMode2D.Impulse);
+                    pFuelBar += 10;
+                }
+            }
+        }
     }
 }
+
